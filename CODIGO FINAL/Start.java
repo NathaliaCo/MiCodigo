830fe2f2a6366ce9bfe467513b821bffadce67df
NathaliaCo <115585344+NathaliaCo@users.noreply.github.com>
2022-11-17 16:50:55 -0500

    Create Start.java
    Arranque (Start)

---
+++ CODIGO FINAL/Start.java
@@ -0,0 +1,13 @@
+package Arranque;
+
+
+import Gestion.gestorVariable;
+
+public class Start {
+
+    public static void main(String[] args) throws InterruptedException {
+        gestorVariable gestor = new gestorVariable();
+        gestor.iniciar();
+    }
+
+}
